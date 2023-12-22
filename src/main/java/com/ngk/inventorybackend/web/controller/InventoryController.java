057480715ee8228d0941a5599340b64fa3b65965
bacallaV <valdohalo3@hotmail.com>
2023-12-22 00:11:43 -0600

    refactor: inventoryController post method

--- src/main/java/com/ngk/inventorybackend/web/controller/InventoryController.java
+++ src/main/java/com/ngk/inventorybackend/web/controller/InventoryController.java
@@ -1,14 +1,20 @@
 package com.ngk.inventorybackend.web.controller;
 
-import com.ngk.inventorybackend.domain.service.InventoryService;
-import com.ngk.inventorybackend.persistence.entity.Inventory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
-import java.util.Optional;
+
+import com.ngk.inventorybackend.domain.service.InventoryService;
+import com.ngk.inventorybackend.persistence.entity.Inventory;
 
 @RestController
 @RequestMapping("/inventories")
@@ -39,7 +45,7 @@ public class InventoryController {
     }
 
     @PostMapping("/create")
-    public ResponseEntity<Inventory> create(Inventory inventory) {
+    public ResponseEntity<Inventory> create(@RequestBody Inventory inventory) {
         return new ResponseEntity<>(this.inventoryService.create(inventory), HttpStatus.OK);
     }
 
